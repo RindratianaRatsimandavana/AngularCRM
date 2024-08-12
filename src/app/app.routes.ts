b99785d8db5862ef4c501a5d6529c44baa61e30a
RindratianaRatsimandavana <rindratianaratsimandavana@gmail.com>
2024-08-12 15:45:36 +0300

    update root

---
+++ src/app/app.routes.ts
@@ -0,0 +1,55 @@
+import { RedirectCommand, Router, Routes, UrlTree } from '@angular/router'
+import { LayoutComponent } from './layouts/layout/layout.component'
+import { Error404Component } from './views/auth/error404/error404.component'
+import { Error500Component } from './views/auth/error500/error500.component'
+import { MaintenanceComponent } from './views/auth/maintenance/maintenance.component'
+import { inject } from '@angular/core'
+import { AuthenticationService } from './core/service/auth.service'
+
+export const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'dashboard/analytics',
+  //   pathMatch: 'full',
+  // },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [
+      () => {
+        const currentUser = inject(AuthenticationService).session;
+        const router: Router = inject(Router);
+        if (currentUser) {
+          // Redirigez vers la page d'accueil que vous voulez pour un utilisateur connecté
+          const urlTree: UrlTree = router.parseUrl('/accueil');  // Changez '/accueil' selon votre page d'accueil
+          return new RedirectCommand(urlTree, { skipLocationChange: true });
+        }
+        // Si non connecté, redirigez vers la page de connexion
+        const urlTree: UrlTree = router.parseUrl('/auth/log-in');
+        return new RedirectCommand(urlTree, { skipLocationChange: true });
+      },
+    ],
+    loadChildren: () =>
+      import('./views/views.route').then((mod) => mod.VIEW_ROUTES),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./views/auth/auth.route').then((mod) => mod.AUTH_ROUTES),
+  },
+  {
+    path: 'not-found',
+    component: Error404Component,
+    data: { title: '404 - Error' },
+  },
+  {
+    path: 'error-500',
+    component: Error500Component,
+    data: { title: '500 - Error' },
+  },
+  {
+    path: 'maintenance',
+    component: MaintenanceComponent,
+    data: { title: 'Maintenance' },
+  },
+]
---
+++ src/app/views/advance_ui/dragula/dragula.component.ts
@@ -0,0 +1,15 @@
+import { currency, currentYear } from '@/app/common/constants'
+import { Component } from '@angular/core'
+import { DragulaModule } from 'ng2-dragula'
+
+@Component({
+  selector: 'app-dragula',
+  standalone: true,
+  imports: [DragulaModule],
+  templateUrl: './dragula.component.html',
+  styles: ``,
+})
+export class DragulaComponent {
+  currency = currency
+  currentYear = currentYear
+}
